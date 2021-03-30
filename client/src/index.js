08b0f4174e913499cdd4ca0bc251f1067808d58b
Tristan <tristanshawnjohnson@gmail.com>
2021-03-30 10:30:24 -0700

    React is now working

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render('React is working', document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World');
-})
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
